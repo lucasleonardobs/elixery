18552a72d76e990caee142b8ee57f3209cd75692
Lucas Leonardo <lucas.leonardo@g.globo>
2022-02-06 18:33:54 -0300

    Create facade in main package to create order

--- lib/elixery.ex
+++ lib/elixery.ex
@@ -1,10 +1,14 @@
 defmodule Elixery do
+  alias Elixery.Orders.Agent, as: OrderAgent
+  alias Elixery.Orders.CreateOrUpdate, as: CreateOrUpdateOrders
   alias Elixery.Users.Agent, as: UserAgent
-  alias Elixery.Users.CreateOrUpdate
+  alias Elixery.Users.CreateOrUpdate, as: CreateOrUpdateUser
 
   def start_agents do
+    OrderAgent.start_link(%{})
     UserAgent.start_link(%{})
   end
 
-  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_order(params), to: CreateOrUpdateOrders, as: :call
 end
