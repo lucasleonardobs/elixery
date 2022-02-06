f0bfa669e47d55fbc30dae0e99677e37e3af9554
Lucas Leonardo <lucas.leonardo@g.globo>
2022-02-06 10:42:48 -0300

    Implement test to build Item

---
+++ test/orders/item_test.exs
@@ -0,0 +1,61 @@
+defmodule Elixery.Orders.ItemTest do
+  use ExUnit.Case
+
+  alias Elixery.Orders.Item
+
+  import Elixery.Factory
+
+  describe "build/4" do
+    test "when all params are valid, return an item" do
+      description = "Bobó de Camarão"
+      category = :prato_feito
+      unity_price = "18.00"
+      quantity = 2
+
+      response = Item.build(description, category, unity_price, quantity)
+
+      expected_response = {:ok, build(:item)}
+
+      assert response == expected_response
+    end
+
+    test "when there is an invalid category, return an error" do
+      description = "Bobó de Camarão"
+      category = :invalid_category
+      unity_price = "18.00"
+      quantity = 2
+
+      response = Item.build(description, category, unity_price, quantity)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+
+    test "when there is an invalid price, return an error" do
+      description = "Bobó de Camarão"
+      category = :prato_feito
+      unity_price = "invalid_price"
+      quantity = 2
+
+      response = Item.build(description, category, unity_price, quantity)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+
+    test "when there is an invalid quantity, return an error" do
+      description = "Bobó de Camarão"
+      category = :prato_feito
+      unity_price = "18.00"
+      quantity = 0
+
+      response = Item.build(description, category, unity_price, quantity)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+  end
+end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,6 +2,7 @@ defmodule Elixery.Factory do
   use ExMachina
 
   alias Elixery.Users.User
+  alias Elixery.Orders.Item
 
   def user_factory do
     %User{
@@ -10,6 +11,15 @@ defmodule Elixery.Factory do
       address: "Rua dos Coqueiros",
       cpf: "12345678900",
       age: 19
+    }
+  end
+
+  def item_factory do
+    %Item{
+      category: :prato_feito,
+      description: "Bobó de Camarão",
+      quantity: 2,
+      unity_price: %Decimal{coef: 1800, exp: -2, sign: 1}
     }
   end
 end
