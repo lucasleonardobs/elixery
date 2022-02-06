deaa5993deb33c8f1e1cd1ebd0e51e2d4d0c9eeb
Lucas Leonardo <lucas.leonardo@g.globo>
2022-02-06 18:35:00 -0300

    Test the User agent

---
+++ test/users/agent_test.exs
@@ -0,0 +1,57 @@
+defmodule Elixery.Users.AgentTest do
+  use ExUnit.Case
+
+  alias Elixery.Users.Agent, as: UserAgent
+
+  import Elixery.Factory
+
+  describe "save/1" do
+    setup do
+      UserAgent.start_link(%{})
+
+      :ok
+    end
+
+    test "saves the user" do
+      user = build(:user)
+
+      response = UserAgent.save(user)
+
+      expected_response = :ok
+
+      assert response == expected_response
+    end
+  end
+
+  describe "get/1" do
+    setup do
+      UserAgent.start_link(%{})
+
+      cpf = "12345678900"
+
+      {:ok, cpf: cpf}
+    end
+
+    test "when the user is found, return the user", %{cpf: cpf} do
+      user = build(:user, cpf: cpf)
+
+      UserAgent.save(user)
+
+      response = UserAgent.get(cpf)
+
+      expected_response = {:ok, user}
+
+      assert response == expected_response
+    end
+
+    test "when the user is not found, return an error" do
+      invalid_cpf = "banana"
+
+      response = UserAgent.get(invalid_cpf)
+
+      expected_response = {:error, "User not found"}
+
+      assert response == expected_response
+    end
+  end
+end
