741a644f8aa34b8de19f48bd5e9100bf9b7fd2a9
Lucas Leonardo <lucas.leonardo@g.globo>
2022-02-06 19:37:57 -0300

    Create a function to generate a report from the orders placed

---
+++ lib/orders/report.ex
@@ -0,0 +1,28 @@
+defmodule Elixery.Orders.Report do
+  alias Elixery.Orders.Agent, as: OrderAgent
+  alias Elixery.Orders.Item
+  alias Elixery.Orders.Order
+
+  def create(filename \\ "report.csv") do
+    order_list = build_order_list()
+
+    File.write(filename, order_list)
+  end
+
+  defp build_order_list() do
+    OrderAgent.index()
+    |> Map.values()
+    |> Enum.map(&order_string(&1))
+    |> IO.inspect()
+  end
+
+  defp order_string(%Order{user_cpf: cpf, items: items, total_price: total_price}) do
+    items_string = Enum.map(items, &item_string(&1))
+
+    "#{cpf},#{items_string},#{total_price}\n"
+  end
+
+  defp item_string(%Item{category: category, quantity: quantity, unity_price: unity_price}) do
+    "#{category},#{quantity},#{unity_price}"
+  end
+end
