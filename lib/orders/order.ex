331b76134168e1953d56f6de45021d366bc43d43
Lucas Leonardo <lucas.leonardo@g.globo>
2022-02-06 09:40:02 -0300

    Create Order struct without validations

---
+++ lib/orders/order.ex
@@ -0,0 +1,18 @@
+defmodule Elixery.Orders.Order do
+  @keys [:user_cpf, :delivery_address, :items, :total_price]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build do
+    {
+      :ok,
+      %__MODULE__{
+        user_cpf: nil,
+        delivery_address: nil,
+        items: nil,
+        total_price: nil
+      }
+    }
+  end
+end
